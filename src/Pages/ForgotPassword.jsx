45004f473dd67633ceac7cb6cbae282ce0e68308
Osuohia Emmanuel <josephe442@gmail.com>
2022-05-07 09:54:01 +0100

    added ForgotPassword functionality

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>
+  const [email, setEmail] = useState('')
+
+  const handleChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success('Password reset email sent')
+    } catch (error) {
+      toast.error('Could not send reset email')
+    }
+  }
+  return (
+    <div className="container mx-auto">
+      <div className="grid place-items-center">
+        <div className="card w-full max-w-2xl shadow bg-base-100 mt-6">
+          <div className="card-title mt-4 ml-8">
+            <h1 className="text-xl">Welcome Back!</h1>
+          </div>
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn bg-neutral">Send Reset Link</button>
+              </div>
+            </form>
+            <div className="divider">OR</div>
+            <div className="form-control mt-6">
+              <Link to="/sign-in" className="btn btn-ghost">
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ForgotPassword
