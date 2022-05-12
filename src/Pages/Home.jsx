0b1d95e9752437f1d688bae7d460407fd9ea0aa6
Osuohia Emmanuel <josephe442@gmail.com>
2022-05-12 16:15:48 +0100

    added home slide feature

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getAuth } from 'firebase/auth'
 import { Link } from 'react-router-dom'
+import HomeSlider from '../components/HomeSlider'
 import HouseIamge1 from '../assets/house-image/houseImage1.jpg'
 import HouseIamge4 from '../assets/house-image/houseImage4.jpg'
 
@@ -16,28 +17,38 @@ function Home() {
   return (
     <>
       <div className="container mx-auto">
-        <h1 className="ml-4 mt-6">Categories</h1>
-        <div className="grid grid-cols-2 gap-8 items-center mr-4 ml-4">
-          <div className="card h-1/2">
-            <Link to="/category/rent" clasName="card-body">
-              <img
-                className="w-full h-full"
-                src={HouseIamge4}
-                alt="houseImage"
-              />
-            </Link>
-          </div>
-          <div className="card h-1/2">
-            <Link to="/category/share" clasName="card-body">
-              <img
-                className="w-full h-full"
-                src={HouseIamge4}
-                alt="houseImage"
-              />
-            </Link>
-            {/* <div className="card-title">
-              <h1 className="font-normal text-sm mt-2 ml-4">House For Share</h1>
-            </div> */}
+        <div className="mx-4">
+          <HomeSlider />
+          <p className="font-bold text-lg my-4">Categories</p>
+          <div className="grid grid-cols-2 gap-8 items-center">
+            <div className="mt-2">
+              <div className="card lg:h-44 md:h-36 sm:h-20">
+                <Link to="/category/rent">
+                  <figure>
+                    <img
+                      className="w-full h-full"
+                      src={HouseIamge4}
+                      alt="houseImage"
+                    />
+                  </figure>
+                </Link>
+              </div>
+              <p className="text-md font-normal my-2">Place for Rent</p>
+            </div>
+            <div className="mt-2">
+              <div className="card lg:h-44 md:h-36 sm:h-20">
+                <Link to="/category/share">
+                  <figure>
+                    <img
+                      className="w-full h-full"
+                      src={HouseIamge4}
+                      alt="houseImage"
+                    />
+                  </figure>
+                </Link>
+              </div>
+              <p className="text-md font-normal my-2">Places for share</p>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { db } from '../firebase.config'
+import Spinner from './Spinner'
+import 'swiper/css/bundle'
+
+function HomeSlider() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingRef = collection(db, 'listings')
+      const q = query(listingRef, orderBy('timestamp', 'desc'), limit(5))
+      const querySnap = await getDocs(q)
+
+      let listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+      setListings(listings)
+      setLoading(false)
+    }
+    fetchListings()
+  }, [])
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  if (listings.length === 0) {
+    return <></>
+  }
+
+  return (
+    listings && (
+      <>
+        <p className='mt-8 mb-2 font-bold text-lg'>Recommended</p>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imageUrls[0]}) center center / cover no-repeat`,
+                }}
+                className="relative w-full h-72"
+              >
+                <p className="absolute top-24 p-2 max-w-full font-bold lg:text-xl md:text-lg bg-neutral text-white">{data.name}</p>
+                <p className="absolute top-36 p-2 max-w-full font-bold text-md bg-white text-neutral">
+                  ${data.discountedPrice ?? data.regularPrice}
+                  {''}
+                  {data.type === 'rent' && ' / month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  )
+}
+
+export default HomeSlider
